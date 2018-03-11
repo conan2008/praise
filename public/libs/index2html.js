bb0b86703ba9e6d049ae25dfff8cb634624131a0
conan2008 <200832342@qq.com>
2018-03-11 09:37:15 +0800

    测试travis

--- public/libs/index2html.js
+++ public/libs/index2html.js
@@ -6,7 +6,7 @@ module.exports = function(templateParams) {
     const _web = webAssetsHelp(templateParams, _cssList);
     const _html = 
         `{% extends 'layout.html' %} 
-        {% block title %}点赞{% endblock %}
+        {% block title %}点赞123{% endblock %}
         {% block styles %}` + _web.styles + `{% endblock %}
         {% block content %}
         {% include './praise.html' %}
