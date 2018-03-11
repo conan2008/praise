2eb94b8d9d63cf0d43a53b6b084782a7e3d8113b
conan2008 <200832342@qq.com>
2018-03-11 10:04:00 +0800

    修改端口

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 var Config = new Map();
-Config.set('port',3002);
+Config.set('port',3389);
 Config.set('staticDir',path.join(__dirname, '..', '..', '/public'));
 Config.set('viewDir',path.join(__dirname, '..', '..', '/public/views'));
 
